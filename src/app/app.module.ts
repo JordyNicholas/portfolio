cbffdb5384f75bbd941a8c258967780cb9d80ea6
Jordy <jordyncdasilva@live.com>
2024-04-23 22:37:27 -0300

    update commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopMenuComponent } from './components/top-menu/top-menu.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { LeftViewComponent } from './left-view/left-view.component';
+import { RightViewComponent } from './right-view/right-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent,
+    SideMenuComponent,
+    LeftViewComponent,
+    RightViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/right-view/right-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-view',
+  templateUrl: './right-view.component.html',
+  styleUrls: ['./right-view.component.css']
+})
+export class RightViewComponent {
+
+}
